8151f25fbab02168964deecd9b0f686a00a0fa78
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-03-14 15:05:53 +0700

    Fix reports and users

--- src/models/reports.model.js
+++ src/models/reports.model.js
@@ -3,47 +3,52 @@
 const Sequelize = require('sequelize');
 const DataTypes = Sequelize.DataTypes;
 
-module.exports = function (app) {
+module.exports = function(app) {
   const sequelizeClient = app.get('sequelizeClient');
-  const reports = sequelizeClient.define('reports', {
-    id_report: {
-      primaryKey: true,
-      autoIncrement: true,
-      type: DataTypes.INTEGER
+  const reports = sequelizeClient.define(
+    'reports',
+    {
+      id_report: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      fullname: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      subject: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      graduated: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      id_major: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      id_faculty: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      }
     },
-    fullname: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    subject: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    major: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    faculty: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    batch: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-  }, {
-    hooks: {
-      beforeCount(options) {
-        options.raw = true;
+    {
+      hooks: {
+        beforeCount(options) {
+          options.raw = true;
+        }
       }
     }
-  });
+  );
 
-  reports.associate = function (models) { // eslint-disable-line no-unused-vars
+  reports.associate = function(models) {
+    // eslint-disable-line no-unused-vars
     // Define associations here
     // See http://docs.sequelizejs.com/en/latest/docs/associations/
   };
--- src/models/users.model.js
+++ src/models/users.model.js
@@ -3,106 +3,116 @@
 const Sequelize = require('sequelize');
 const DataTypes = Sequelize.DataTypes;
 
-module.exports = function (app) {
+module.exports = function(app) {
   const sequelizeClient = app.get('sequelizeClient');
-  const users = sequelizeClient.define('users', {
-    nim: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      unique: true
+  const users = sequelizeClient.define(
+    'users',
+    {
+      nim: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      kecamatan: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      kelurahan: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      provinsi: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      postcode: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      gender: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      birth: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      birth_place: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      avatar: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      graduated: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      id_major: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      id_faculty: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      },
+      facebook: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      twitter: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      linkedin: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      verified: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+      }
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    kecamatan: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    kelurahan: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    provinsi: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    postcode: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    gender: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    birth: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    birth_place: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    avatar: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    graduated: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    id_major: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    id_faculty: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    facebook: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    twitter: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    linkedin: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    verified: {
-      type: DataTypes.BOOLEAN,
-      allowNull: true
-    }
-  }, {
-    hooks: {
-      beforeCount(options) {
-        options.raw = true;
+    {
+      hooks: {
+        beforeCount(options) {
+          options.raw = true;
+        }
       }
     }
-  });
+  );
 
-  users.associate = function (models) { // eslint-disable-line no-unused-vars
+  users.associate = function(models) {
+    // eslint-disable-line no-unused-vars
     // Define associations here
-    users.hasMany(models.majors, {foreignKey: 'id_major', sourceKey: 'id_major'});
-    users.hasMany(models.faculties, {foreignKey: 'id_faculty', sourceKey: 'id_faculty'});
+    users.hasMany(models.majors, {
+      foreignKey: 'id_major',
+      sourceKey: 'id_major'
+    });
+    users.hasMany(models.faculties, {
+      foreignKey: 'id_faculty',
+      sourceKey: 'id_faculty'
+    });
     // See http://docs.sequelizejs.com/en/latest/docs/associations/
   };
 
